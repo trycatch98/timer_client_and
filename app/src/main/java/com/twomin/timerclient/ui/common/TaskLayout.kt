48751d0d21e4435f6c2c83c2354c4007383fb69d
trycatch <trycatch98@gmail.com>
2020-10-20 20:59:56 +0900

    feat : main ui 구성

---
+++ app/src/main/java/com/twomin/timerclient/ui/common/TaskLayout.kt
@@ -0,0 +1,9 @@
+package com.twomin.timerclient.ui.common
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class TaskLayout @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr)
