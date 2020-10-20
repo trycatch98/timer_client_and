3395b0d0e9072485ad3266ad70545911d38dbddb
trycatch <trycatch98@gmail.com>
2020-10-20 21:19:29 +0900

    refactor : TimerDecoration에서 dimen으로부터 값을 가져오도록 변경

--- app/src/main/java/com/twomin/timerclient/ui/main/TimerDecoration.kt
+++ app/src/main/java/com/twomin/timerclient/ui/main/TimerDecoration.kt
@@ -3,6 +3,7 @@ package com.twomin.timerclient.ui.main
 import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.twomin.timerclient.R
 
 class TimerDecoration : RecyclerView.ItemDecoration() {
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
@@ -10,6 +11,6 @@ class TimerDecoration : RecyclerView.ItemDecoration() {
         val adapter = parent.adapter ?: return
         if (parent.getChildAdapterPosition(view) == adapter.itemCount - 1)
             return
-        outRect.bottom = (15 * view.resources.displayMetrics.density).toInt()
+        outRect.bottom = view.resources.getDimension(R.dimen.itemOffset).toInt()
     }
 }
